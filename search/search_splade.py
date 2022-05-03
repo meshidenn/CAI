45fe7f5731f79e1c7e0613848ad4a8ddaafcc3c3
meshidenn <mesitahiro@hotmail.com>
2022-05-03 20:32:39 +0900

    change args

--- search/search_splade.py
+++ search/search_splade.py
@@ -57,17 +57,17 @@ def main(args):
 
     out_results = {}
     analysis = {}
-    for k in calc_models:
-        beir_splade = calc_models[k]
-        dres = DRES(beir_splade, batch_size=args.batch_size, corpus_chunk_size=args.corpus_chunk_size)
-        retriever = EvaluateRetrieval(dres, score_function="dot", k_values=k_values)
-        results = retriever.retrieve(corpus, queries)
-        ndcg, map_, recall, p = EvaluateRetrieval.evaluate(qrels, results, k_values)
-        results2 = EvaluateRetrieval.evaluate_custom(qrels, results, k_values, metric="r_cap")
-        res = {"NDCG@10": ndcg["NDCG@10"], "Recall@100": recall["Recall@100"], "R_cap@100": results2["R_cap@100"]}
-        out_results[k] = res
-        analysis[k] = results
-        print("{} model result:".format(k), res, flush=True)
+    mode = args.mode
+    beir_splade = calc_models[mode]
+    dres = DRES(beir_splade, batch_size=args.batch_size, corpus_chunk_size=args.corpus_chunk_size)
+    retriever = EvaluateRetrieval(dres, score_function="dot", k_values=k_values)
+    results = retriever.retrieve(corpus, queries)
+    ndcg, map_, recall, p = EvaluateRetrieval.evaluate(qrels, results, k_values)
+    results2 = EvaluateRetrieval.evaluate_custom(qrels, results, k_values, metric="r_cap")
+    res = {"NDCG@10": ndcg["NDCG@10"], "Recall@100": recall["Recall@100"], "R_cap@100": results2["R_cap@100"]}
+    out_results[mode] = res
+    analysis[mode] = results
+    print("{} model result:".format(mode), res, flush=True)
 
     with open(out_path, "w") as f:
         json.dump(out_results, f)
@@ -82,8 +82,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_type_or_dir")
     parser.add_argument("--data_dir")
     parser.add_argument("--out_dir")
-    parser.add_argument("--load_weight", type=strtobool)
-    parser.add_argument("--weight_sqrt", type=strtobool)
+    parser.add_argument("--mode", default="org", help="org, idf, bm25")
     parser.add_argument("--batch_size", default=128, type=int)
     parser.add_argument("--corpus_chunk_size", default=50000, type=int)
 
